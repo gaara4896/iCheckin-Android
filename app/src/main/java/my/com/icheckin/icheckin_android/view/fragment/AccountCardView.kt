cb6d37d2df15c5dc437beaf55b8e44890c49c9b0
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2018-01-31 13:55:02 +0800

    Add show crudentials

---
+++ app/src/main/java/my/com/icheckin/icheckin_android/view/fragment/AccountCardView.kt
@@ -0,0 +1,38 @@
+package my.com.icheckin.icheckin_android.view.fragment
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pawegio.kandroid.visible
+import kotlinx.android.synthetic.main.cardview_account.view.*
+import my.com.icheckin.icheckin_android.R
+import my.com.icheckin.icheckin_android.model.Student
+
+/**
+ * Created by gaara on 1/31/18.
+ */
+class AccountCardView(val context: Context, val students: MutableList<Student>, val listener: (Student) -> Unit) : RecyclerView.Adapter<AccountCardView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountCardView.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.cardview_account, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: AccountCardView.ViewHolder, position: Int) {
+        holder.bindItems(context, students[position], position, listener)
+    }
+
+    override fun getItemCount(): Int {
+        return students.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(context: Context, student: Student, position: Int, listener: (Student) -> Unit) = with(itemView) {
+            itemView.textView_ID.text = student.username
+            if (position == 0) itemView.imageView_Crown.visible = true
+        }
+    }
+
+}
--- app/src/main/java/my/com/icheckin/icheckin_android/view/fragment/AccountFragment.kt
+++ app/src/main/java/my/com/icheckin/icheckin_android/view/fragment/AccountFragment.kt
@@ -1,15 +1,20 @@
 package my.com.icheckin.icheckin_android.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pawegio.kandroid.IntentFor
 import kotlinx.android.synthetic.main.fragment_account.*
 import my.com.icheckin.icheckin_android.R
+import my.com.icheckin.icheckin_android.model.Student
+import my.com.icheckin.icheckin_android.utils.database.Database
 import my.com.icheckin.icheckin_android.view.AddAccountActivity
+import my.com.icheckin.icheckin_android.view.fragment.AccountCardView
 
 
 /**
@@ -29,6 +34,19 @@ class AccountFragment : Fragment() {
         button_NewAccount.setOnClickListener {
             startActivity(IntentFor<AddAccountActivity>(context))
         }
+
+        loadStudent()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadStudent()
+    }
+
+    fun loadStudent(){
+        val students = Database.query<Student>(activity.applicationContext, Student())
+        recycleView_Account.layoutManager = LinearLayoutManager(activity.applicationContext)
+        recycleView_Account.adapter = AccountCardView(activity.applicationContext, students) {}
     }
 
 }// Required empty public constructor
