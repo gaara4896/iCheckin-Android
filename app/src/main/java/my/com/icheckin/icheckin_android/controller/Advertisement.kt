7f43152c4fb91c87e8a8f4ba00fa85f98d05f5f2
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2018-03-17 17:58:04 +0800

    Update ads id and version

--- app/src/main/java/my/com/icheckin/icheckin_android/controller/Advertisement.kt
+++ app/src/main/java/my/com/icheckin/icheckin_android/controller/Advertisement.kt
@@ -4,9 +4,9 @@ package my.com.icheckin.icheckin_android.controller
  * Created by gaara on 3/16/18.
  */
 object Advertisement {
-    val ADMOB_APP_ID = "ca-app-pub-3940256099942544~3347511713"
-    val BANNER_ADS_ID = "ca-app-pub-3940256099942544/6300978111"
-    val INTERSTITIALS_ADS_ID = "ca-app-pub-3940256099942544/1033173712"
-    val REWARD_VIDEO_ID = "ca-app-pub-3940256099942544/5224354917"
-    val NATIVE_ADS_ID = "ca-app-pub-3940256099942544/2247696110"
+    val ADMOB_APP_ID = "ca-app-pub-7921227984437156~4908497709"
+    val BANNER_ADS_ID = "ca-app-pub-7921227984437156/3022902090"
+    val INTERSTITIALS_ADS_ID = "ca-app-pub-7921227984437156/9175045778"
+    // val REWARD_VIDEO_ID = "ca-app-pub-3940256099942544/5224354917"
+    // val NATIVE_ADS_ID = "ca-app-pub-3940256099942544/2247696110"
 }
